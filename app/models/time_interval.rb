276544bf452037fbac17f4933d72c701f78b6b07
William Holmberg <william.av.holmberg@gmail.com>
2018-11-19 11:15:05 +0100

    asd

--- app/models/time_interval.rb
+++ app/models/time_interval.rb
@@ -1,3 +1,15 @@
 class TimeInterval < ApplicationRecord
   belongs_to :schema
+
+
+  def get_task_duration
+    return (self.end_time - self.start_time)/60
+  end
+
+  def get_start_time
+    return self.start_time - 3600
+  end
+  def get_end_time
+    return self.end_time - 3600
+  end
 end

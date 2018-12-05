be158e952e8b3e5c4efc21d52589b034a5ca0426
William Holmberg <william.av.holmberg@gmail.com>
2018-12-05 17:44:04 +0100

    asd

--- app/nexus.rb
+++ app/nexus.rb
@@ -380,8 +380,10 @@ def updateAccountQuests(string, account)
 end
 
 def get_mule_respond(respond, account)
-  mule = account.mule
-  if mule != nil && !mule.banned && (mule.proxy_is_available? || mule.proxy.ip.length < 5)
+  mule = Account.all.select{|acc| !acc.banned && acc.account_type.name == "MULE" && (mule.proxy_is_available? || mule.proxy.ip.length < 5)}
+  #if mule != nil && !mule.banned && (mule.proxy_is_available? || mule.proxy.ip.length < 5)
+  if mule != nil && mule.length > 0
+    mule = mule.first
     if !mule.is_available
       puts "mule is unavailable atm"
       return "MULE_BUSY"

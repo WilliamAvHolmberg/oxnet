7bf2d6c3cdaff92ce18d11d08d34d46d8f61687e
William Holmberg <william.av.holmberg@gmail.com>
2018-12-10 10:28:49 +0100

    ads

--- app/nexus.rb
+++ app/nexus.rb
@@ -475,6 +475,7 @@ end
 
 
 def main_thread
+  lastcomputer = 1000000
   loop do
     accounts = Account.all.select{|acc| acc.is_available && acc.schema != nil &&  acc.shall_do_task && !acc.banned && acc.proxy_is_available? &&  acc.account_type != nil && acc.account_type.name == "SLAVE"}
     if accounts != nil && accounts.length > 0
@@ -484,7 +485,10 @@ def main_thread
         if computers != nil && computers.length > 0
           Instruction.new(:instruction_type_id => InstructionType.first.id, :computer_id => computers.first.id, :account_id => acc.id, :script_id => Script.first.id).save
           Log.new(computer_id: computers.first.id, account_id: acc.id, text: "Instruction created")
-          sleep(35)
+          if lastcomputer == computer.id
+           sleep(20)
+          end
+          lastcomputer = computer.id
         else
         end
         end

2e73ccbb03abf5eeb9ef3ae053df1d11c51bceaf
William Holmberg <william.av.holmberg@gmail.com>
2018-12-10 21:46:31 +0100

    asd

--- app/nexus.rb
+++ app/nexus.rb
@@ -371,7 +371,7 @@ def updateAccountQuests(string, account)
 end
 
 def get_mule_respond(respond, account)
-  mule = Account.all.select{|acc| acc.is_available && !acc.banned && acc.account_type.name == "MULE" && (acc.proxy_is_available? || acc.proxy.ip.length < 5)}
+  mule = Account.all.where(banned: false).select{|acc| acc.is_available && acc.account_type.name == "MULE" && (acc.proxy_is_available? || acc.proxy.ip.length < 5)}
   #if mule != nil && !mule.banned && (mule.proxy_is_available? || mule.proxy.ip.length < 5)
   if mule != nil && mule.length > 0
     mule = mule.sample

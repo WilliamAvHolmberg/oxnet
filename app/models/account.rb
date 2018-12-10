50c13301753aa43c35203495ad4e46a347b378c1
William Holmberg <william.av.holmberg@gmail.com>
2018-12-10 21:19:12 +0100

    asdf

--- app/models/account.rb
+++ app/models/account.rb
@@ -47,8 +47,8 @@ class Account < ApplicationRecord
   end
 
   def is_available
-    #if difference is larger than 5 minutes  we can suppose that the account is not logged in
-    return time_since_last_log > 1
+    #if difference is larger than 6 minutes  we can assume that the account is not logged in
+    return time_since_last_log > 6
   end
 
   def get_time_online

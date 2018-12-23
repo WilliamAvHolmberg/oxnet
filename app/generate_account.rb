0f44ec9b36bbbe18728e115fb9b757443a7225bd
William Holmberg <william.av.holmberg@gmail.com>
2018-12-23 02:22:32 +0100

    asd

--- app/generate_account.rb
+++ app/generate_account.rb
@@ -160,15 +160,16 @@ class GenerateAccount
   #todo fix size (13 atm)
   public
     def create_accounts_for_all_computers
+      should_do = true
       computers = find_available_computers
       computers.each do |computer|
         account_threshold = computer.max_slaves
         current_amount_of_accounts = get_available_accounts_on_computer(computer)
-        if current_amount_of_accounts != nil && current_amount_of_accounts.size < account_threshold
+        if should_do && current_amount_of_accounts != nil && current_amount_of_accounts.size < account_threshold
           puts current_amount_of_accounts.size
             create_account(computer, true)
             #puts "lets create acc for #{computer.name}"
-            break
+            should_do = false
         end
       end
     end

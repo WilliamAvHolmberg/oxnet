28902afbf080106f432977e617f2787348a84ea9
William Holmberg <william.av.holmberg@gmail.com>
2019-01-08 20:24:13 +0100

    Revert "Hie"
    This reverts commit 93194e61d859b030f8ccb8d2c7f3045259bb410e.

--- app/nexus.rb
+++ app/nexus.rb
@@ -597,8 +597,9 @@ def main_thread
   begin
   loop do
     puts "Main Thread loop"
-    all_accounts = Account.limit(30).where(banned: false, created: true).select{|acc| acc.is_available && acc.schema != nil &&  acc.shall_do_task && !acc.banned && acc.proxy_is_available? &&  acc.account_type != nil && acc.account_type.name == "SLAVE"}
+    all_accounts = Account.where(banned: false, created: true).select{|acc| acc.is_available && acc.schema != nil &&  acc.shall_do_task && !acc.banned && acc.proxy_is_available? &&  acc.account_type != nil && acc.account_type.name == "SLAVE"}
     accounts = all_accounts.sort_by{|acc|acc.get_total_level}.reverse
+    puts "after start thread"
     if accounts != nil && accounts.length > 0
       accounts.each do |acc|
       computer = acc.computer if acc.computer_id != nil

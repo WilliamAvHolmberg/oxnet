8f32045bcd5d6a2dfe84fd358d24204a9b08f106
William Holmberg <william.av.holmberg@gmail.com>
2019-03-01 19:42:59 +0100

    Database and setup for db - test env

---
+++ app/testing/setup_for_db.rb
@@ -0,0 +1,38 @@
+require_relative 'generate_items'
+require_relative 'generate_worlds'
+require_relative 'generate_slots'
+
+
+
+
+
+#generate account types
+AccountType.create(name: "SLAVE").save
+AccountType.create(name: "MULE").save
+
+#TaskTypes
+TaskType.create(name: "MULE_DEPOSIT").save
+TaskType.create(name: "MULE_WITHDRAW").save
+TaskType.create(name: "MULE_DEPOSIT").save
+TaskType.create(name: "WOODCUTTING").save
+TaskType.create(name: "COMBAT").save
+TaskType.create(name: "AGILITY").save
+TaskType.create(name: "QUEST").save
+
+
+#break condition
+BreakCondition.create(name: "TIME").save
+BreakCondition.create(name: "LEVEL").save
+BreakCondition.create(name: "TIME_OR_LEVEL").save
+
+#instructions
+InstructionType.create(name: "NEW_CLIENT").save
+InstructionType.create(name: "MULE_WITHDRAW").save
+InstructionType.create(name: "CREATE_ACCOUNT").save
+
+#gen items
+GenerateItems.new
+#gen worlds
+GenerateWorlds.new
+#gen slots
+GenerateSlots.new

a24045bcccf12dc96168ef73b28fad5a6fb5fd2b
William Holmberg <william.av.holmberg@gmail.com>
2019-03-20 00:36:32 +0100

    Test

--- app/generate_account.rb
+++ app/generate_account.rb
@@ -195,7 +195,7 @@ class GenerateAccount
       schema = Schema.next_to_use
       mule = Account.where(username: "PetDhaL").first #not needed. random
       account_type = "SLAVE"
-      if get_number_of_mules < (computer.max_slaves / 30).round
+      if get_number_of_mules < 7
         account_type = "MULE"
         other_mule = Account.where(account_type: AccountType.where(:name => "MULE")).first
         if other_mule != nil

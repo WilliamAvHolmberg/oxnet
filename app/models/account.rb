67d39fdfae08459b208446da97bdac220207c29e
William Holmberg <william.av.holmberg@gmail.com>
2019-04-17 13:52:47 +0200

    extended unavailable worlds

--- app/models/account.rb
+++ app/models/account.rb
@@ -240,7 +240,7 @@ class Account < ApplicationRecord
     end
     end
   def get_least_used_worlds
-    @unavailable_worlds = [302, 308, 309, 310, 316, 317, 318, 325, 326, 333, 334, 341, 342, 349, 350, 358, 364, 365, 366, 371, 372, 373, 379, 380, 381, 382, 407, 408]
+    @unavailable_worlds = [302, 308, 309, 310, 316, 317, 318, 325, 326, 333, 334, 341, 342, 349, 350, 358, 364, 365, 366, 371, 372, 373, 379, 380, 381, 382, 407, 408,497,498,499,500,501,502,503,504]
 
     rs_worlds = RsWorld.where(members_only: false)
     worlds = Array.new
@@ -263,7 +263,8 @@ class Account < ApplicationRecord
 
 
   def get_world
-    @unavailable_worlds = [302, 308, 309, 310, 316, 317, 318, 325, 326, 333, 334, 341, 342, 349, 350, 358, 364, 365, 366, 371, 372, 373, 379, 380, 381, 382, 407, 408]
+    @unavailable_worlds = [302, 308, 309, 310, 316, 317, 318, 325, 326, 333, 334, 341, 342, 349, 350, 358, 364, 365, 366, 371, 372, 373, 379, 380, 381, 382, 407, 408,497,498,499,500,501,502,503,504]
+
 
     current_world = world.chomp.to_i
     if @unavailable_worlds.include?(current_world)

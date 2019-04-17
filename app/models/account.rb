b87963d6b53dddf9d490d1cd27459044522cafe1
William Holmberg <william.av.holmberg@gmail.com>
2019-04-17 13:50:23 +0200

    typo

--- app/models/account.rb
+++ app/models/account.rb
@@ -238,9 +238,10 @@ class Account < ApplicationRecord
     else
       puts "No accounts to launch"
     end
-  end
-  @unavailable_worlds = [302, 308, 309, 310, 316, 317, 318, 325, 326, 333, 334, 341, 342, 349, 350, 358, 364, 365, 366, 371, 372, 373, 379, 380, 381, 382, 407, 408]
+    end
   def get_least_used_worlds
+    @unavailable_worlds = [302, 308, 309, 310, 316, 317, 318, 325, 326, 333, 334, 341, 342, 349, 350, 358, 364, 365, 366, 371, 372, 373, 379, 380, 381, 382, 407, 408]
+
     rs_worlds = RsWorld.where(members_only: false)
     worlds = Array.new
     current_lowest = 10000
@@ -262,6 +263,8 @@ class Account < ApplicationRecord
 
 
   def get_world
+    @unavailable_worlds = [302, 308, 309, 310, 316, 317, 318, 325, 326, 333, 334, 341, 342, 349, 350, 358, 364, 365, 366, 371, 372, 373, 379, 380, 381, 382, 407, 408]
+
     current_world = world.chomp.to_i
     if @unavailable_worlds.include?(current_world)
       new_world = get_least_used_worlds.sample
